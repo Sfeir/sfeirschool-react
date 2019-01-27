cfbc779aef6bc894eaf0ecbeccbeddb74e9e717e
Wolfgang Goedel <wgoedel@gmail.com>
2019-01-27 11:39:39 +0100

    ex08 example custom hook

--- src/ex09/Player.js
+++ src/ex09/Player.js
@@ -1,7 +1,46 @@
-import React from "react";
-import { WithPeople } from "./PeopleContext";
-import { Player as WrappedPlayer } from "../solution/Player";
+import React, { useState, useEffect } from "react";
+import { Fab } from "@rmwc/fab";
+import { Carousel } from "../solution/Carousel";
+import { PersonCard } from "../solution/PersonCard";
+import { usePeople } from "./PeopleContext";
 
-export const Player = () => (
-  <WithPeople>{({ people }) => <WrappedPlayer people={people} />}</WithPeople>
-);
+const useScheduler = (f, interval) => {
+  const [running, setRunning] = useState(false);
+  const toggleRunning = () => {
+    !running && f();
+    setRunning(!running);
+  };
+  useEffect(
+    () => {
+      if (running) {
+        const iid = setInterval(f, interval);
+        return () => clearInterval(iid);
+      }
+    },
+    [running]
+  );
+  return { running, toggleRunning };
+};
+
+export const Player = () => {
+  const { people } = usePeople();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const showNextPerson = () => setCurrentIndex(i => i + 1);
+  const { running, toggleRunning } = useScheduler(showNextPerson, 1000);
+
+  return (
+    <>
+      <main>
+        <Carousel currentIndex={currentIndex} onChange={setCurrentIndex}>
+          {people.map(person => (
+            <PersonCard person={person} key={person.id} />
+          ))}
+        </Carousel>
+      </main>
+      <footer>
+        <Fab icon={running ? "pause" : "play_arrow"} onClick={toggleRunning} />
+      </footer>
+    </>
+  );
+};
