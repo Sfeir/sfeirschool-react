8779c0ace850bea3f0a8548026dd126479580c05
Wolfgang Goedel <wgoedel@gmail.com>
2019-03-26 17:17:46 +0100

    add and configure redux middleware

--- src/solution/main.js
+++ src/solution/main.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
+import { apiMiddleware } from "redux-api-middleware";
 import { Provider } from "react-redux";
 
 import { Config } from "./Config";
 import { App } from "./App";
 import { reducer } from "./state";
 
-const store = createStore(reducer, devToolsEnhancer());
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunkMiddleware, apiMiddleware))
+);
 
 render(
   <Config useRouter>
